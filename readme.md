7b6b5c05703340ef1af19d4f873754780ed60e29
Paulo Imon <3463932+pauloimon@users.noreply.github.com>
2024-03-27 19:13:28 -0300

    Remove dependency of Unsafe Mode

--- readme.md
+++ readme.md
@@ -3,7 +3,3 @@
 A simple shortcut to take screenshots directly on your top bar!
 
 ![Screenshot](screenshot.png)
-
-## Disclaimer
-
-Since GNOME 41, it's necessary to enable Unsafe Mode to perform method calls via the D-Bus interface. This extension will automatically enable it to call the native screenshot UI. Use at your own risk.
--- src/extension.js
+++ src/extension.js
@@ -2,12 +2,11 @@ const St = imports.gi.St;
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
-const GLib = imports.gi.GLib;
 
 function init() {
   return new class {
     enable() {
-      this._panelMenuButton = new PanelMenu.Button(0.0, Me.metadata.name, false);
+      this._panelMenuButton = new PanelMenu.Button(0, Me.metadata.name, false);
 
       this._panelMenuButton.add_child(
         new St.Icon({
@@ -19,24 +18,13 @@ function init() {
       Main.panel.addToStatusArea(Me.metadata.uuid, this._panelMenuButton);
 
       this._panelMenuButton.connect('button-press-event', function () {
-        GLib.spawn_command_line_async(`
-          gdbus call \
-          --session \
-          --dest org.gnome.Shell \
-          --object-path /org/gnome/Shell \
-          --method org.gnome.Shell.Eval \
-          "Main.screenshotUI.open();"
-        `);
+        Main.screenshotUI.open();
       });
-
-      global.context.unsafe_mode = true;
     }
 
     disable() {
       this._panelMenuButton?.destroy();
       this._panelMenuButton = null;
-
-      global.context.unsafe_mode = false;
     }
   };
 }
