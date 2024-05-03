63e233171454e536b8ad7066fdb26210e3812517
Paulo Imon <3463932+pauloimon@users.noreply.github.com>
2024-05-03 01:18:47 -0300

    Add support for GNOME 45 and 46

--- src/extension.js
+++ src/extension.js
@@ -1,30 +1,29 @@
-const St = imports.gi.St;
-const Me = imports.misc.extensionUtils.getCurrentExtension();
-const Main = imports.ui.main;
-const PanelMenu = imports.ui.panelMenu;
+import St from 'gi://St';
 
-function init() {
-  return new class {
-    enable() {
-      this._panelMenuButton = new PanelMenu.Button(0, Me.metadata.name, false);
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 
-      this._panelMenuButton.add_child(
-        new St.Icon({
-          icon_name: 'camera-photo-symbolic',
-          style_class: 'system-status-icon',
-        })
-      );
+export default class extends Extension {
+  enable() {
+    this._panelMenuButton = new PanelMenu.Button(0, this.metadata.name, false);
 
-      Main.panel.addToStatusArea(Me.metadata.uuid, this._panelMenuButton);
+    this._panelMenuButton.add_child(
+      new St.Icon({
+        icon_name: 'camera-photo-symbolic',
+        style_class: 'system-status-icon',
+      })
+    );
 
-      this._panelMenuButton.connect('button-press-event', function () {
-        Main.screenshotUI.open();
-      });
-    }
+    Main.panel.addToStatusArea(this.uuid, this._panelMenuButton);
 
-    disable() {
-      this._panelMenuButton?.destroy();
-      this._panelMenuButton = null;
-    }
-  };
+    this._panelMenuButton.connect('button-press-event', function () {
+      Main.screenshotUI.open();
+    });
+  }
+
+  disable() {
+    this._panelMenuButton?.destroy();
+    this._panelMenuButton = null;
+  }
 }
